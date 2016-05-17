65aff3a80e413d4cc48797c6e855c44e8ad8d338
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2016-05-17 13:50:00 +0200

    Add script to update year in copyright header

---
+++ copyright_header/update_year.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+OLD_YEAR='2015'
+NEW_YEAR='2016'
+grep -rli "2010-$OLD_YEAR" * | xargs -i@ sed -i "s/2010-$OLD_YEAR/2010-$NEW_YEAR/" @
+grep -rli "2013-$OLD_YEAR" * | xargs -i@ sed -i "s/2013-$OLD_YEAR/2013-$NEW_YEAR/" @
