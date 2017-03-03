f6d85e5ad1236304f8840216f92b9b29612feaad
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-03-03 09:35:42 +0100

    Increment year in update_year.sh

--- copyright_header/update_year.sh
+++ copyright_header/update_year.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OLD_YEAR='2015'
-NEW_YEAR='2016'
+OLD_YEAR='2016'
+NEW_YEAR='2017'
 grep -rli "2010-$OLD_YEAR" * | xargs -i@ sed -i "s/2010-$OLD_YEAR/2010-$NEW_YEAR/" @
 grep -rli "2013-$OLD_YEAR" * | xargs -i@ sed -i "s/2013-$OLD_YEAR/2013-$NEW_YEAR/" @
